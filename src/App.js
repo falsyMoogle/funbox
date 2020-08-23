9f7aef236d8a89a7be98fe65cf63db978c634bc5
falsyMoogle <mgsmile1990@gmail.com>
2020-08-23 21:13:27 +0300

    Update Card component. Update styles.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Card from './components/Card'
 function App() {
 	return (
 		<div className='App'>
-			<div className='page-container flex-center'>
-				<Card />
+			<div className='page-container flex-column'>
+				<span className='title'>Ты сегодня покормил кота?</span>
+				<div className='flex-center'>
+					<Card />
+					<Card />
+					<Card />
+				</div>
 			</div>
 		</div>
 	)
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
-import '../styles/components/Card.scss'
-
 const Card = () => {
 	return (
 		<div className='Card'>
 			<div className='Card__background'>
 				<div className='Card__content'>
-					<div className='Card__content-image'></div>
+					<div className='Card__content__desc'>
+						<p className='Card__content__desc-title'>
+							Сказочное заморское яство
+						</p>
+						<p className='Card__content__desc-brand'>Нямушка</p>
+						<p className='Card__content__desc-taste'>с фуа-гра</p>
+						<p className='Card__content__desc-options'>
+							10 порций <br /> мышь в подарок
+						</p>
+					</div>
+					<div className='Card__content__image'>
+						<img src={require('../assets/img/Cat.png')} alt='Cat' />
+					</div>
 				</div>
 			</div>
-			<span className='Card__description'>Description</span>
+			<p className='Card__description'>Чего сидишь? Порадуй котэ, купи.</p>
 		</div>
 	)
 }
