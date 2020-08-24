04cc3c5a1ff45774eb83433cb88cbceb8317395e
falsyMoogle <mgsmile1990@gmail.com>
2020-08-24 21:19:56 +0300

    Update title media query.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,7 +5,7 @@ import Card from './Card'
 const CardList = ({ products }) => {
 	return (
 		<div className='page-container flex-column'>
-			<span className='title'>Ты сегодня покормил кота?</span>
+			<span className='list-title'>Ты сегодня покормил кота?</span>
 			<div className='flex-center'>
 				{products.map(product => (
 					<Card key={product.id} product={product} />
